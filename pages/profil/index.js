4a4b118cd1f84dd04df5d330b15ad408ccf6afd7
huertatas <max.54.1990@gmail.com>
2022-01-13 11:12:31 +0100

    no cors

--- pages/profil/index.js
+++ pages/profil/index.js
@@ -41,6 +41,7 @@ function ProfilePage() {
 
     fetch(`${process.env.API_URL}api/v1/update/${userCtx.userId}`, {
       method: "PUT",
+      mode: "no-cors",
       headers: {
         "Content-type": "application/json",
         authorization: `${userCtx.userToken}`,
@@ -62,6 +63,7 @@ function ProfilePage() {
         headers: {
           authorization: `${userCtx.userToken}`,
         },
+        mode: "no-cors",
       })
         .then((res) => {
           res.json().then((data) => {
