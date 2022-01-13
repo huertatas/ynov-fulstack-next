e272487355a7a83925055d15f73f9bcb86689dd3
huertatas <max.54.1990@gmail.com>
2022-01-13 12:49:26 +0100

    ajout checkout page

--- components/header/Cart.js
+++ components/header/Cart.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Cart() {
-  return <Image src="/cart.svg" alt="" height={50} width={50}></Image>;
+  return (
+    <Link href="/shop/checkout">
+      <Image src="/cart.svg" className="cart" alt="" height={50} width={50}></Image>
+    </Link>
+  );
 }
 
 export default Cart;
---
+++ pages/shop/checkout/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Checkout() {
+  return (
+    <div className="checkout-container">
+      <div className="checkout-block">checkout</div>
+    </div>
+  );
+}
+
+export default Checkout;
