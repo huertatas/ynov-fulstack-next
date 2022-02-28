784623f6276c6638d3dd0b6616e8211348a1f053
huertatas <max.54.1990@gmail.com>
2022-02-28 19:27:21 +0100

    test for deploy

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   env: {
     API_URL: process.env.API_URL,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
